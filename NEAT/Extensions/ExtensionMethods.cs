5a2a473e5d698d6f95ff36c9cbfb7dcb520adcd7
Neil Francis Nahid <nahidnoriel@gmail.com>
2020-03-31 18:15:25 +0800

    moved static class to NEAT folder

--- NEAT/Extensions/ExtensionMethods.cs
+++ NEAT/Extensions/ExtensionMethods.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Linq;
-
+using NEAT.NEAT;
 namespace NEAT.ExtensionMethods
 {
     public static class ExtensionMethods
--- NEAT/NEAT/Static.cs
+++ NEAT/NEAT/Static.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace NEAT
+namespace NEAT.NEAT
 {
     public static class Static
     {
